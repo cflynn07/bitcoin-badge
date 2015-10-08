ea1b3706690043057352c175cdadc9f8a704522b
Casey Flynn <cflynn.us@gmail.com>
2015-10-07 19:32:26 -0700

    Add http request listener callback that returns string

--- index.go
+++ index.go
@@ -1,6 +1,19 @@
+// https://golang.org/doc/articles/wiki/
 package main
 
 import (
 	"fmt"
 	"net/http"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there!")
+}
+
+/**
+ *
+ */
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
