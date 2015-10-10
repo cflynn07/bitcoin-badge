db91f1694da92409d87d5c6b773178366aacbaf1
Casey Flynn <cflynn.us@gmail.com>
2015-10-10 11:51:22 -0700

    Add explanatory godoc block comment detail

--- index.go
+++ index.go
@@ -1,6 +1,10 @@
 /*
 Every Go package should have a block comment at the top of the file. godoc
 will extract for documentation.
+
+This is a sandbox project for me to play with Go. Eventually I would like
+to create a server that returns an image "badge" for a given Bitcoin
+address that contains information such as balance, recieved, sent, etc.
 */
 package main
 
